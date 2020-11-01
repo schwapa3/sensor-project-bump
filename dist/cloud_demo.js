7810af19b810ab97b596370a4723da6993f6188a
schwapa3 <schwapa3@students.zhaw.ch>
2020-11-01 15:11:42 +0100

    ugga ugga

--- dist/cloud_demo.js
+++ dist/cloud_demo.js
@@ -3,13 +3,23 @@ var rootUrl = window.location.origin; // get the root URL, e.g. https://example.
 var app = new Vue({
     el: "#app",
     data: {
-        buttonState_0: "unknown", // the state of the button on device 0
-        buttonState_1: "unknown", // the state of the button on device 1
-        buttonPressCounter: 0,    // how many times the buttons were pressed
-        buttonsSync: false,       // true if the buttons were pressed within 1 second
-        blinking_0: false,        // true if device 0 is blinking.
-        blinking_1: false,        // true if device 0 is blinking.
-        // add your own variables here ...
+        a1_0: "unknown", // the state of the button on device 0
+        a2_0: "unknown",
+        b1_0: "unknown",
+        b2_0: "unknown",
+        c1_0: "unknown",
+        c2_0: "unknown",
+        t_0: "unknown",
+        a1_1: "unknown", // the state of the button on device 0
+        a2_1: "unknown",
+        b1_1: "unknown",
+        b2_1: "unknown",
+        c1_1: "unknown",
+        c2_1: "unknown",
+        t_1: "unknown",
+        bumpCounter: 0,    // how many times the buttons were pressed
+        bumpSync: false,       // true if the buttons were pressed within 1 second
+
     },
     // This function is executed once when the page is loaded.
     mounted: function () {
@@ -31,64 +41,167 @@ var app = new Vue({
         },
         // react on events: update the variables to be displayed
         updateVariables(ev) {
-            // Event "buttonStateChanged"
-            if (ev.eventName === "buttonStateChanged") {
-                this.buttonPressCounter = ev.eventData.counter;
-                if (ev.eventData.message === "pressed") {
-                    this.buttonsSync = ev.eventData.pressedSync;
-                }
+            // Event "bump"
+            if (ev.eventName === "bump") {
+                this.bumpCounter = ev.eventData.counter;
+                this.bumpSync = ev.eventData.bumpSync;
             }
-            // Event "blinkingStateChanged"
-            else if (ev.eventName === "blinkingStateChanged") {
-                if (ev.eventData.message === "started blinking") {
-                    if (ev.deviceNumber === 0) {
-                        this.blinking_0 = true;
-                    }
-                    else if (ev.deviceNumber === 1) {
-                        this.blinking_1 = true;
-                    }
-                }
-                if (ev.eventData.message === "stopped blinking") {
-                    if (ev.deviceNumber === 0) {
-                        this.blinking_0 = false;
-                    }
-                    else if (ev.deviceNumber === 1) {
-                        this.blinking_1 = false;
-                    }
-                }
-            }
+
         },
         // call the function "blinkRed" in your backend
-        blinkRed: function (nr) {
+        remoteBump: function (nr) {
             var duration = 2000; // blinking duration in milliseconds
-            axios.post(rootUrl + "/api/device/" + nr + "/function/blinkRed", { arg: duration })
+            if(nr == 2) {
+                axios.post(rootUrl + "/api/device/0/function/remoteBump", { arg: duration })
                 .then(response => {
                     // Handle the response from the server
                     console.log(response.data); // we could to something meaningful with the return value here ... 
                 })
                 .catch(error => {
-                    alert("Could not call the function 'blinkRed' of device number " + nr + ".\n\n" + error)
+                    alert("Could not call the function 'remoteBump' of device number 0.\n\n" + error)
                 })
-        },
-        // get the value of the variable "buttonState" on the device with number "nr" from your backend
-        getButtonState: function (nr) {
-            axios.get(rootUrl + "/api/device/" + nr + "/variable/buttonState")
+                axios.post(rootUrl + "/api/device/1/function/remoteBump", { arg: duration })
                 .then(response => {
                     // Handle the response from the server
-                    var buttonState = response.data.result;
+                    console.log(response.data); // we could to something meaningful with the return value here ... 
+                })
+                .catch(error => {
+                    alert("Could not call the function 'remoteBump' of device number 1.\n\n" + error)
+                })
+            }else{
+            axios.post(rootUrl + "/api/device/" + nr + "/function/remoteBump", { arg: duration })
+                .then(response => {
+                    // Handle the response from the server
+                    console.log(response.data); // we could to something meaningful with the return value here ... 
+                })
+                .catch(error => {
+                    alert("Could not call the function 'remoteBump' of device number " + nr + ".\n\n" + error)
+                })
+            }
+            
+        },
+        // get the value of the variable "buttonState" on the device with number "nr" from your backend
+        getVariables: function (nr) {
+            axios.get(rootUrl + "/api/device/" + nr + "/variable/a1")
+                .then(response => {
+                    // Handle the response from the server
+                    var a1 = response.data.result;
                     if (nr === 0) {
-                        this.buttonState_0 = buttonState;
+                        this.a1_0 = a1;
                     }
                     else if (nr === 1) {
-                        this.buttonState_1 = buttonState;
+                        this.a1_1 = a1;
                     }
                     else {
                         console.log("unknown device number: " + nr);
                     }
                 })
                 .catch(error => {
-                    alert("Could not read the button state of device number " + nr + ".\n\n" + error)
+                    alert("Could not read variable a1 of device number " + nr + ".\n\n" + error)
                 })
+                axios.get(rootUrl + "/api/device/" + nr + "/variable/a2")
+                .then(response => {
+                    // Handle the response from the server
+                    var a2 = response.data.result;
+                    if (nr === 0) {
+                        this.a2_0 = a2;
+                    }
+                    else if (nr === 1) {
+                        this.a2_1 = a2;
+                    }
+                    else {
+                        console.log("unknown device number: " + nr);
+                    }
+                })
+                .catch(error => {
+                    alert("Could not read variable a2 of device number " + nr + ".\n\n" + error)
+                })
+                axios.get(rootUrl + "/api/device/" + nr + "/variable/b1")
+                .then(response => {
+                    // Handle the response from the server
+                    var b1 = response.data.result;
+                    if (nr === 0) {
+                        this.b1_0 = b1;
+                    }
+                    else if (nr === 1) {
+                        this.b1_1 = b1;
+                    }
+                    else {
+                        console.log("unknown device number: " + nr);
+                    }
+                })
+                .catch(error => {
+                    alert("Could not read variable b1 of device number " + nr + ".\n\n" + error)
+                })
+                axios.get(rootUrl + "/api/device/" + nr + "/variable/b2")
+                .then(response => {
+                    // Handle the response from the server
+                    var b2 = response.data.result;
+                    if (nr === 0) {
+                        this.b2_0 = b2;
+                    }
+                    else if (nr === 1) {
+                        this.b2_1 = b2;
+                    }
+                    else {
+                        console.log("unknown device number: " + nr);
+                    }
+                })
+                .catch(error => {
+                    alert("Could not read tvariable b2 of device number " + nr + ".\n\n" + error)
+                })
+                axios.get(rootUrl + "/api/device/" + nr + "/variable/c1")
+                .then(response => {
+                    // Handle the response from the server
+                    var c1 = response.data.result;
+                    if (nr === 0) {
+                        this.c1_0 = c1;
+                    }
+                    else if (nr === 1) {
+                        this.c1_1 = c1;
+                    }
+                    else {
+                        console.log("unknown device number: " + nr);
+                    }
+                })
+                .catch(error => {
+                    alert("Could not read variable c1 of device number " + nr + ".\n\n" + error)
+                })
+                axios.get(rootUrl + "/api/device/" + nr + "/variable/c2")
+                .then(response => {
+                    // Handle the response from the server
+                    var c2 = response.data.result;
+                    if (nr === 0) {
+                        this.c2_0 = c2;
+                    }
+                    else if (nr === 1) {
+                        this.c2_1 = c2;
+                    }
+                    else {
+                        console.log("unknown device number: " + nr);
+                    }
+                })
+                .catch(error => {
+                    alert("Could not read variable c2 of device number " + nr + ".\n\n" + error)
+                })
+                axios.get(rootUrl + "/api/device/" + nr + "/variable/t")
+                .then(response => {
+                    // Handle the response from the server
+                    var t = response.data.result;
+                    if (nr === 0) {
+                        this.t_0 = t;
+                    }
+                    else if (nr === 1) {
+                        this.t_1 = t;
+                    }
+                    else {
+                        console.log("unknown device number: " + nr);
+                    }
+                })
+                .catch(error => {
+                    alert("Could not read variable t of device number " + nr + ".\n\n" + error)
+                })
+                
         }
     }
 })
--- eventListeners.js
+++ eventListeners.js
@@ -1,72 +1,46 @@
 // remember the last event so that we can check if two buttons were pressed within 1 second
-var lastButtonPressEvent = {
+var lastBumpEvent = {
     deviceId: "",
     timestamp: 0
 }
 
 // remember how many times the buttons were pressed
-var buttonPressCounter = 0;
+var bumpCounter = 0;
 
-// react on the "blinkingStateChanged" Event
-function handleBlinkingStateChanged (event) {
+
+// react on the "bump" Event
+function handleBump (event) {
     // read variables from the event
     let ev = JSON.parse(event.data);
-    let evData = ev.data; // the data from the argon event: "started blinking" or "stopped blinking"
+    let evData = ev.data; // the data from the argon event
     let evDeviceId = ev.coreid; // the device id
     let evTimestamp = Date.parse(ev.published_at); // the timestamp of the event
 
-    // the data we want to send to the clients
-    let data = {
-        message: evData, // just forward "started blinking" or "stopped blinking"
-    }
+    bumpCounter++;
 
-    // send data to all connected clients
-    sendData("blinkingStateChanged", data, evDeviceId, evTimestamp );
-}
-
-// react on the "buttonStateChanged" Event
-function handleButtonStateChanged (event) {
-    // read variables from the event
-    let ev = JSON.parse(event.data);
-    let evData = ev.data; // the data from the argon event: "pressed" or "released"
-    let evDeviceId = ev.coreid; // the device id
-    let evTimestamp = Date.parse(ev.published_at); // the timestamp of the event
-
-    // helper variables that we need to build the message to be sent to the clients
     let sync = false;
-    let msg = "";
 
-    if (evData === "pressed") {
-        buttonPressCounter++; // increase the buttonPressCounter by 1
-        msg = "pressed";
-
-        // check if the last two button press events were whithin 1 second
-        if (evTimestamp - lastButtonPressEvent.timestamp < 1000) {
-            if (evDeviceId !== lastButtonPressEvent.deviceId) {
-                sync = true;
-            }
+    if (evTimestamp - lastBumpEvent.timestamp < 1000) {
+        if (evDeviceId !== lastBumpEvent.deviceId) {
+            sync = true;
         }
+    }
 
-        lastButtonPressEvent.timestamp = evTimestamp;
-        lastButtonPressEvent.deviceId = evDeviceId;
-    } 
-    else if (evData === "released") {
-        msg = "released";
-    }
-    else {
-        msg = "unknown state";
-    }
+    lastBumpEvent.timestamp = evTimestamp;
+    lastBumpEvent.deviceId = evDeviceId;
+
 
     // the data we want to send to the clients
     let data = {
-        message: msg,
-        counter: buttonPressCounter,
-        pressedSync: sync
+        message: evData, // just forward "bumped"
+        counter: bumpCounter,
+        bumpSync: sync
     }
 
     // send data to all connected clients
-    sendData("buttonStateChanged", data, evDeviceId, evTimestamp );
+    sendData("bump", data, evDeviceId, evTimestamp );
 }
+
 
 // send data to the clients.
 // You don't have to change this function
@@ -91,5 +65,4 @@ exports.deviceIds = [];
 exports.sse = null;
 
 // export your own functions here as well
-exports.handleBlinkingStateChanged = handleBlinkingStateChanged;
-exports.handleButtonStateChanged = handleButtonStateChanged;
+exports.handleBump = handleBump;
